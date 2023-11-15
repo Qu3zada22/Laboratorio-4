d401196146e89170b39154c159fc95e2a0d0130e
Ihan-Marroquin <ihanmarroquin12@gmail.com>
2023-11-14 22:31:12 -0600

    Se agrego la funcionalidad para cambiar entre usuario base a premium

--- POO_2023_Laboratorio4-main/Plan.java
+++ POO_2023_Laboratorio4-main/Plan.java
@@ -1,3 +1,20 @@
-public enum Plan {
-    BASE, PREMIUM
+public class Plan {
+    private String nombre;
+    private boolean premium;
+
+    private Plan(String nombre, boolean premium) {
+        this.nombre = nombre;
+        this.premium = premium;
+    }
+
+    public static Plan BASE = new Plan("Base", false);
+    public static Plan PREMIUM = new Plan("Premium", true);
+
+    public boolean esPremium() {
+        return premium;
+    }
+
+    public String toString() {
+        return nombre;
+    }
 }
--- POO_2023_Laboratorio4-main/Revista.java
+++ POO_2023_Laboratorio4-main/Revista.java
@@ -1,4 +1,4 @@
-package POO_2023_Laboratorio4-main;
+
 
 public class Revista extends Producto {
     public Revista(String nombre, double precio) {
